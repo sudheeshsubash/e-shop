f16dc406014a33d9eb898b188e01dfc8691e6d0e
Myshadowss <109855548+Myshadowss@users.noreply.github.com>
2023-05-26 15:07:19 +0530

    sql lite data base set

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -97,19 +97,19 @@ WSGI_APPLICATION = 'eshop_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
     # mysql
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eshop',
-        'USER':'root',
-        'PASSWORD':'admin',
-        'HOST':'127.0.0.1',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'eshop',
+    #     'USER':'root',
+    #     'PASSWORD':'admin',
+    #     'HOST':'127.0.0.1',
+    # }
 
 }
 
